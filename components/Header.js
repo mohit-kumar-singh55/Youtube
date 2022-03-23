77631938371dbe534d882f44c119cebee0ca5e15
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-23 21:10:51 +0530

    only user image left

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
-import { MenuIcon, SearchIcon } from "@heroicons/react/outline";
+import { BellIcon, MenuIcon, SearchIcon, VideoCameraIcon, ViewGridIcon } from "@heroicons/react/outline";
+import { MicrophoneIcon } from "@heroicons/react/solid";
 import youtube from "../public/assets/youtube.png";
 
 const Header = () => {
@@ -10,24 +11,28 @@ const Header = () => {
             <div className='flex justify-between items-center w-full gap-10 p-2 px-8'>
                 {/* Left */}
                 <div className='flex items-center justify-between'>
-                    <MenuIcon className='w-7 h-7' />
-                    <div className='relative w-24 h-10 ml-6'>
+                    <MenuIcon className='w-7 h-7 cursor-pointer' />
+                    <div className='relative w-24 h-10 ml-6 cursor-pointer'>
                         <Image src={youtube} alt="logo" className='invert' objectFit='contain' layout='fill' />
                     </div>
                 </div>
 
                 {/* Center */}
-                <div className='w-[600px]'>
-                    <div className='bg-[#313131] p-[0.8px] rounded-sm flex items-center justify-center'>
-                        <input className='w-full bg-[#121212] p-4 h-10 rounded-sm block sm:text-sm focus:border-gray-50' type="Search" placeholder='Search' />
+                <div className='w-[600px] flex items-center justify-between gap-4'>
+                    <div className='bg-[#313131] p-[0.8px] rounded-sm flex items-center justify-center flex-1 cursor-pointer'>
+                        <input className='w-full bg-[#121212] p-4 h-10 rounded-sm focus:border-gray-50' type="Search" placeholder='Search here...' />
                         <SearchIcon className='w-6 h-6 mx-5' />
                     </div>
+                    <span className='bg-[#181818] rounded-full p-2'>
+                        <MicrophoneIcon className='w-6 h-6 cursor-pointer' />
+                    </span>
                 </div>
 
                 {/* Right */}
-                <div className="flex items-center justify-end space-x-4">
-                    {/* <HomeIcon onClick={handleClick} className='navbtn' /> */}
-                    <MenuIcon className='h-6' />
+                <div className="flex items-center justify-end gap-6">
+                    <VideoCameraIcon className='h-6 stroke-1 cursor-pointer' />
+                    <ViewGridIcon className='h-6 stroke-1 cursor-pointer' />
+                    <BellIcon className='h-6 stroke-1 cursor-pointer' />
                 </div>
             </div>
         </nav>
