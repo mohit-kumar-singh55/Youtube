fb604c89f31f7939171b2555f64cb8c35578c353
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-23 18:30:52 +0530

    Refactoring

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        Youtube Clonee
+      </main>
+
+    </div>
+  )
+}
+
+export default Home
