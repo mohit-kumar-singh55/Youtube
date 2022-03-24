f117ca89d5b168d91d05f95107b1142c1b02b58d
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-24 19:47:41 +0530

    sdf

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { HomeIcon } from "@heroicons/react/solid";
 
 const Sidebar = () => {
     return (
-        <div className='w-[250px] bg-[#202020] h-screen overflow-y-scroll'>
-            
-        </div>
+        <aside className='w-[250px] bg-[#202020] h-screen overflow-y-scroll flex items-center justify-start flex-col gap-5 py-2'>
+            {/* sections */}
+            <div className='w-full flex items-center justify-start flex-col'>
+                <div className='w-full flex items-center justify-start hover:bg-[#4C4C4C] py-2 px-6 gap-6 cursor-pointer'>
+                    <HomeIcon className='w-6 h-6' />
+                    <p className='font-semibold leading-8'>Home</p>
+                </div>
+            </div>
+        </aside>
     )
 }
-
 export default Sidebar;
