56722ee643d359efd43a00e2b60a7f49d4635b53
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-24 20:53:59 +0530

    Half Sidebar Completed

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HomeIcon } from "@heroicons/react/solid";
+import { CashIcon, ColorSwatchIcon, CubeTransparentIcon, DesktopComputerIcon, DocumentDuplicateIcon, DuplicateIcon, HomeIcon, LibraryIcon, RefreshIcon } from "@heroicons/react/solid";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
@@ -7,16 +7,69 @@ const Sidebar = () => {
     const router = useRouter();
 
     return (
-        <aside className='w-[250px] bg-[#202020] h-screen overflow-y-scroll flex items-center justify-start flex-col gap-5 py-2'>
-            {/* sections */}
-            <div className='w-full flex items-center justify-start flex-col'>
+        <aside className='w-[250px] bg-[#202020] h-screen overflow-y-scroll flex items-center justify-start flex-col gap-1'>
+            {/* section */}
+            <div className='w-full flex items-center justify-start flex-col py-2'>
+                {/* Home */}
                 <Link href="/">
-                    <div className='w-full flex items-center justify-start hover:bg-[#4C4C4C] py-2 px-6 gap-6 cursor-pointer'>
+                    <div className={`w-full flex items-center justify-start ${router.pathname === "/" ? "bg-[#363636]" : ""} hover:bg-[#4C4C4C] transition duration-300 py-2 px-6 gap-6 cursor-pointer`}>
                         <HomeIcon className='w-6 h-6' />
                         <p className='font-semibold leading-8'>Home</p>
                     </div>
                 </Link>
+                {/* Explore */}
+                <Link href="/feed/explore">
+                    <div className={`w-full flex items-center justify-start ${router.pathname === "/feed/explore" ? "bg-[#363636]" : ""} hover:bg-[#4C4C4C] transition duration-300 py-2 px-6 gap-6 cursor-pointer`}>
+                        <DesktopComputerIcon className='w-6 h-6' />
+                        <p className='font-semibold leading-8'>Explore</p>
+                    </div>
+                </Link>
+                {/* Shorts */}
+                <Link href="/feed/shorts">
+                    <div className={`w-full flex items-center justify-start ${router.pathname === "/feed/shorts" ? "bg-[#363636]" : ""} hover:bg-[#4C4C4C] transition duration-300 py-2 px-6 gap-6 cursor-pointer`}>
+                        <CubeTransparentIcon className='w-6 h-6' />
+                        <p className='font-semibold leading-8'>Shorts</p>
+                    </div>
+                </Link>
+                {/* Subscription */}
+                <Link href="/feed/subscriptions">
+                    <div className={`w-full flex items-center justify-start ${router.pathname === "/feed/subscriptions" ? "bg-[#363636]" : ""} hover:bg-[#4C4C4C] transition duration-300 py-2 px-6 gap-6 cursor-pointer`}>
+                        <CashIcon className='w-6 h-6' />
+                        <p className='font-semibold leading-8'>Subscription</p>
+                    </div>
+                </Link>
             </div>
+
+            {/* Divider */}
+            <span className='w-[85%] bg-[rgba(255,255,255,0.1)] h-[1px]' />
+
+            {/* section */}
+            <div className='w-full flex items-center justify-start flex-col py-2'>
+                {/* Library */}
+                <Link href="/feed/library">
+                    <div className={`w-full flex items-center justify-start ${router.pathname === "/feed/library" ? "bg-[#363636]" : ""} hover:bg-[#4C4C4C] transition duration-300 py-2 px-6 gap-6 cursor-pointer`}>
+                        <LibraryIcon className='w-6 h-6' />
+                        <p className='font-semibold leading-8'>Library</p>
+                    </div>
+                </Link>
+                {/* History */}
+                <Link href="/feed/history">
+                    <div className={`w-full flex items-center justify-start ${router.pathname === "/feed/history" ? "bg-[#363636]" : ""} hover:bg-[#4C4C4C] transition duration-300 py-2 px-6 gap-6 cursor-pointer`}>
+                        <RefreshIcon className='w-6 h-6' />
+                        <p className='font-semibold leading-8'>History</p>
+                    </div>
+                </Link>
+                {/* Playlists */}
+                <Link href="/playlist/${id}">
+                    <div className={`w-full flex items-center justify-start ${router.pathname === "/playlist/id" ? "bg-[#363636]" : ""} hover:bg-[#4C4C4C] transition duration-300 py-2 px-6 gap-6 cursor-pointer`}>
+                        <ColorSwatchIcon className='w-6 h-6' />
+                        <p className='font-semibold leading-8'>Render Playlists</p>
+                    </div>
+                </Link>
+            </div>
+
+            {/* Divider */}
+            <span className='w-[85%] bg-[rgba(255,255,255,0.1)] h-[1px]' />
         </aside>
     )
 }
